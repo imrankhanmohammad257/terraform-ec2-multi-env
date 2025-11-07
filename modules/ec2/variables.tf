54d819069f7404750ea08bcdf7a0ed04754048e2
MOHAMMAD IMRAN KHAN <imrankhanmohammad257@gmail.com>
2025-11-07 23:32:03 +0530

    Add EC2 variables for AMI, instance type, and key name

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  description = "AMI ID for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key to access EC2"
+  type        = string
+}
