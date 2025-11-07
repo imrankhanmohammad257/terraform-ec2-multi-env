32ecb05fca2ed92fe3e7fcb0df21f5937f7e9c84
MOHAMMAD IMRAN KHAN <imrankhanmohammad257@gmail.com>
2025-11-07 23:31:25 +0530

    Add AWS EC2 instance resource configuration

---
+++ modules/ec2/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "this" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name        = "${var.environment}-ec2"
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
