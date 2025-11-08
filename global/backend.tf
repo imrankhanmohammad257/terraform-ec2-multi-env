2ac30f0fbf42761df83a2706e8d10af3a701ca08
MOHAMMAD IMRAN KHAN <imrankhanmohammad257@gmail.com>
2025-11-08 07:19:16 +0530

    Updated Terraform backend config and variable values for dev environment

--- global/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "company-terraform-states"
-    key            = "ec2/${var.environment}/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
